f66f4d6cc3be38a4f51a70527305698d5fe7b432
Matheus <matheus.dev94@gmail.com>
2024-02-08 12:40:34 -0300

    add hooks to access local storage and auth context

--- app/src/hooks/useLocalStorage.js
+++ app/src/hooks/useLocalStorage.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 const useLocalStorage = (key, initValue) => {
   const [value, setValue] = useState(() => {
     const storedValue = localStorage.getItem(key);
-    alert(storedValue);
     return storedValue ? JSON.parse(storedValue) : initValue;
   });
   useEffect(() => {
