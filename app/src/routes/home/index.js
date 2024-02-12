57cc4bcbb067ceb3be041907dc2b4692bd1afd90
Matheus <matheus.dev94@gmail.com>
2024-02-12 12:01:25 -0300

    stage: add navigation to home screen

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,11 +1,22 @@
 // src/components/Home.js
 import React from "react";
 
+const handleSignOut = () => {
+  console.log("DO THE SIGN OUT");
+};
 const Home = () => {
   return (
     <div>
       <h2>Home Page</h2>
-      <p>Bem-vindo à página inicial!</p>
+      <p>Bem-vindo, {"NAME"}</p>
+      <br />
+      <a href="/editor">Editor</a>
+      <br />
+      <a href="/admin">Admin</a>
+      <br />
+      <a href="/lounge">Lounge</a>
+      <br />
+      <button onClick={() => handleSignOut}>Sign out</button>
     </div>
   );
 };
