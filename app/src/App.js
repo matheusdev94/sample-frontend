fa17a56001e6bfc46d453bb2e4ef8a06c5a22973
Matheus <matheus.dev94@gmail.com>
2024-03-09 18:19:44 -0300

    add: Routes, it has: roles check, persistLogin check, user log in check

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,24 @@
 import "./App.css";
-// import { Components } from "./Components/Components";
-import { Components } from "./Components";
 import React from "react";
-import { Provider } from "react-redux";
-import store from "./store/store";
+// import { Provider } from "react-redux";
+// import store from "./store/store";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Login from "./routes/login";
-import Register from "./routes/register";
-import Home from "./routes/home";
-import Unauthorized from "./routes/unauthorized";
-import Editor from "./routes/editor";
-import Admin from "./routes/admin";
-import Lounge from "./routes/lounge";
-import Missing from "./routes/missing";
+import Layout from "./routes/Layout";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+import Home from "./routes/Home";
+import Unauthorized from "./routes/Unauthorized";
+import Editor from "./routes/Editor";
+import Admin from "./routes/Admin";
+import Lounge from "./routes/Lounge";
+import Missing from "./routes/Missing";
 import RequireAuth from "./Components/RequireAuth";
-
+import PersistLogin from "./Components/PersistLogin";
+import Link from "./routes/Link";
 const ROLES = {
   User: 2001,
   Editor: 1984,
-  Admin: 1984,
+  Admin: 5150,
 };
 
 function App() {
@@ -30,23 +29,29 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="link" element={<Link />} />
 
         {/* protected routes */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/" element={<Home />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+            <Route path="editor" element={<Editor />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route path="admin" element={<Admin />} />
+          </Route>
+          <Route
+            element={
+              <RequireAuth
+                allowedRoles={[ROLES.Admin, ROLES.User, ROLES.Editor]}
+              />
+            }
+          >
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
         </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-          <Route path="editor" element={<Editor />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-          <Route path="admin" element={<Admin />} />
-        </Route>
-        <Route
-          element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor]} />}
-        >
-          <Route path="lounge" element={<Lounge />} />
-        </Route>
-
         {/* call all */}
         <Route path="*" element={<Missing />} />
       </Route>
@@ -55,6 +60,3 @@ function App() {
 }
 
 export default App;
-{
-  /* <Components /> */
-}
