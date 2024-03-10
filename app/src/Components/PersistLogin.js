c9b150aa7961c4550919943650549bde879dbd26
Matheus <matheus.dev94@gmail.com>
2024-03-10 00:15:47 -0300

    fixed persist logic

--- app/src/Components/PersistLogin.js
+++ app/src/Components/PersistLogin.js
@@ -6,33 +6,23 @@ import useLocalStorage from "../hooks/useLocalStorage";
 
 const PersistLogin = () => {
   const { auth } = useAuth();
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [persist] = useLocalStorage("persist", false);
   const refresh = useRefreshToken();
   useEffect(() => {
-    let isMounted = true;
     const verifyRefreshToken = async () => {
       try {
+        setIsLoading(true);
         await refresh();
       } catch (err) {
         console.error(err);
       } finally {
         setIsLoading(false);
-        isMounted && setIsLoading(false);
       }
     };
     !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
-    return () => {
-      isMounted = false;
-    };
   }, []);
-
-  // useEffect(() => {
-  //   // alert(`on Pers.Log::: auth = ${JSON.stringify(auth)}`);
-  // }, [isLoading]);
-
   return (
-    // <>{ isLoading ? <p>Loading...</p> : <Outlet />}</>
     <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
   );
 };
