b618ab6e1af189f21e14cc9159bd859a47c036c9
Matheus <matheus.dev94@gmail.com>
2024-03-11 16:50:00 -0300

    layout improovemnt

--- app/src/hooks/useRefreshToken.js
+++ app/src/hooks/useRefreshToken.js
@@ -4,19 +4,22 @@ import useAuth from "./useAuth";
 const useRefreshToken = () => {
   const { setAuth } = useAuth();
   const refresh = async () => {
-    const response = await axios.get("/refresh", {
-      withCredentials: true,
-    });
-
-    setAuth((prev) => {
-      return {
-        ...prev,
-        accessToken: response?.data?.accessToken,
-        roles: response?.data?.roles,
-        username: response?.data?.username,
-      };
-    });
-    return response?.data?.accessToken;
+    try {
+      const response = await axios.get("/refresh", {
+        withCredentials: true,
+      });
+      setAuth((prev) => {
+        return {
+          ...prev,
+          accessToken: response?.data?.accessToken,
+          roles: response?.data?.roles,
+          username: response?.data?.username,
+        };
+      });
+      return response?.data?.accessToken;
+    } catch (e) {
+      console.error(e);
+    }
   };
   return refresh;
 };
--- app/src/routes/Link.js
+++ app/src/routes/Link.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link as Links, useNavigate } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
-
+import "./Link.css";
+import useAuth from "../hooks/useAuth";
+import useRefreshToken from "../hooks/useRefreshToken";
 const Link = () => {
+  const { auth } = useAuth();
   const navigate = useNavigate();
   const logout = useLogout();
-
+  const refresh = useRefreshToken();
+  useEffect(() => {
+    try {
+      refresh();
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
   const signOut = async () => {
     await logout();
-    navigate("/link");
+    navigate("/login");
   };
+
   return (
     <div className="content">
       <h1>Links</h1>
       <h2>Public</h2>
-      <Links className="link" to="/register">
+      <Links
+        className={auth.accessToken ? "link-disabled" : "link"}
+        to="/register"
+      >
         Register
       </Links>
-      <Links className="link" to="/login">
+      <Links
+        className={auth.accessToken ? "link-disabled" : "link"}
+        to="/login"
+      >
         Login
       </Links>
 
