3a8bb335547863f0ed6b6c1868c93260ccc93940
Matheus <matheus.dev94@gmail.com>
2024-03-15 16:05:39 -0300

    add loading to prevent login page in logged users while fetching accessToken

--- app/src/Components/RequireAuth.js
+++ app/src/Components/RequireAuth.js
@@ -1,10 +1,29 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { useEffect, useState } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import Loading from "../routes/Loading";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
-  return auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
+
+  const [loading, setLoading] = useState(true);
+
+  const refreshToken = useRefreshToken();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      await refreshToken().then(() => {
+        setLoading(false);
+      });
+    };
+    fetchUserData();
+  }, []);
+
+  return loading ? (
+    <Loading />
+  ) : auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
     <Outlet />
   ) : auth?.accessToken ? (
     <Navigate to="/unauthorized" state={{ from: location }} replace />
