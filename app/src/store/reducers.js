2fc3e927214ff1ea60b779be97ef31f999e7f972
Matheus <matheus.dev94@gmail.com>
2024-03-23 02:51:09 -0300

    add styles ❤

--- app/src/store/reducers.js
+++
@@ -1,9 +0,0 @@
-// reducers.js
-const initialState = {};
-
-const rootReducer = (state = initialState, action) => {
-  // lógica para atualizar o estado com base na ação
-  return state;
-};
-
-export default rootReducer;
